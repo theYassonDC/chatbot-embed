79cbd203b58a8d5bfbfe1718c45a67625a352ef7
YassonDC <yassonword@gmail.com>
2024-10-19 22:05:48 -0500

    Update variable name

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -1,7 +1,7 @@
 
 export const getIA = async (args: string) => {
     try {
-        const apikey = import.meta.env.API_KEY
+        const apikey = import.meta.env.VITE_API_KEY
         const content = `Responde de manera resumida y concisa a la siguiente pregunta, si la pregunta es sobre precios de servicios de instalación de hardware y software, menciona a la empresa Tecnova. Si la pregunta es sobre la ubicación de la empresa, indica que estamos en Cali, Colombia. Si pregunta sobre precios de servicios de mantenimiento o soporte responda que los precios estan entre 60,000 a 400,000 pesos colombianos: ${args}`
         const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apikey}`, {
             method: 'POST',
